58682c22c124930f74d66b98fb6306008f978735
shawnagohel <shawna.gohel@gmail.com>
2020-10-08 11:47:41 -0700

    branch

--- dispatch/htmlDispatch.js
+++ dispatch/htmlDispatch.js
@@ -4,16 +4,16 @@ var path = require("path");
 module.exports = function(app) {
   // GET Requests
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
   });
 
   // Link to css file.
   app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/css/styles.css"));
+    res.sendFile(path.join(__dirname, "./Develop/public/styles.css"));
   });
 
   // If no matching route is found default to home page.
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
   });
 };
